c938d5f1527f85da88924545cafe3d1dc5a89065
liyc <38707145@qq.com>
2023-05-10 23:10:37 +0800

    更新

--- forge-admin/src/test/java/net/koodar/forge/admin/ForgeAdminApplicationTests.java
+++ forge-admin/src/test/java/net/koodar/forge/admin/ForgeAdminApplicationTests.java
@@ -1,9 +1,7 @@
 package net.koodar.forge.admin;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ForgeAdminApplicationTests {
 
 	@Test
